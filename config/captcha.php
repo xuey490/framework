8c6387d82534efa502271f066d4b392e946626c8
xuey490 <xuey863toy@gmail.com>
2025-10-21 21:07:53 +0800

    更换字体

--- config/captcha.php
+++ config/captcha.php
@@ -10,7 +10,7 @@ return [
     'height' => 40,
     'font_size' => 16,
     'font_path' => __DIR__ . '/fonts/arial.ttf', // 请确保字体文件存在
-    'chinese_font_path' => __DIR__ . '/fonts/fangzhengxiaobiao.ttf', // 中文字体
+    'chinese_font_path' => __DIR__ . '/fonts/SmileySans-Oblique.ttf', // 中文字体
     'noise' => true, // 干扰点
     'lines' => true, // 干扰线
     'distortion' => true, // 文字扭曲
